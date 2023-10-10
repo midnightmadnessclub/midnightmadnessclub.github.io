222c1fa176b8219333ed4d826e873245fc145285
midnightmadnessclub <666Midnightmadness@gmail.com>
2023-10-11 00:38:28 +1100

    Revamp

--- scripts/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const mainNav = document.getElementById('main-nav');
-    const menuToggle = document.getElementById('menu-toggle');
-    const menuClose = document.getElementById('menu-close');
-
-    menuToggle.onclick = () => mainNav.style.display = 'block';
-    menuClose.onclick = () => mainNav.style.display = 'none';
-});
